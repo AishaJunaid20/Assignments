74887ce8b4f383c7b8575b975fdcb081180f0357
Aisha Junaid <ayeshajunaidmemon@gmail.com>
2025-04-07 11:37:58 +0500

    Update main.py

--- Assignment 4/Lesson 8.py/main.py
+++ Assignment 4/Lesson 8.py/main.py
@@ -1,6 +1,10 @@
-# my_module.py (Custom Module)
-def greet(name):
-    return f"Hello, {name}!"
+# main.py
 
-def add(x, y):
-    return x + y
+import my_module
+
+name = input("Enter your name: ")
+print(my_module.greet(name))
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The sum is:", my_module.add(a, b))
